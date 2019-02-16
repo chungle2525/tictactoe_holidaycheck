72f31e389f33a512db3411d7d56fd3da8bd29724
chungle <chungle@umich.edu>
2019-02-16 14:12:46 -0500

    cleaned up code and added function to randomly choose starting player

--- README.md
+++ README.md
@@ -2,24 +2,23 @@
 tic-tac-toe challenge for Holidaycheck recruiting process
 
 This python program plays tic-tac-toe against the user. The program prints the
-3x3 tic-tac-toe board to the terminal after each move.
+3x3 tic-tac-toe board to the terminal after each move. The user always makes
+the first move.
 The program can be started with:
 > python3 play_tictactoe.py
 
-The user can make a move by typing in the row (1-3) and column (A-C) of the
-desired position in the terminal. Here are some possible moves:
+The program will randomly choose who makes the first move.
+When prompted, the user can make a move by typing in the row (1-3) and column
+(A-C) of the desired position in the terminal. Here are some possible moves:
 > 3A
 > 2C
 > 1b
 > 3c
 
-If a user enters an invalid position (e.g. 5X, 4b, B2), the following message
-will appear:
-> Please enter a valid board position in the format RC, where R is the row
-(1-3) and C is the column (A-C)
+If a user enters an invalid position (e.g. 5X, 4b, B2), they will be notified
+and asked to enter a valid move.
 
-
-The positions for each board position are shown below:
+The names for each board position are shown below:
 --------------------------------
 |		  |          |         |
 |   1A    |    1B    |    1C   |
--- play_tictactoe.py
+++ play_tictactoe.py
@@ -2,9 +2,9 @@ import random
 
 def play_game():
 	print_welcome()
-	# initialize array of size 9 of taken moves
-	taken = ['-', '-', '-', '-', '-', '-', '-', '-', '-']
-	print_board(taken)
+	# initialize the board array of size 9. '-' indicates an empty spot
+	board = ['-', '-', '-', '-', '-', '-', '-', '-', '-']
+	print_board(board)
 	num_moves = 0
 	winning_combinations = [(0, 1, 2),
 							(3, 4, 5),
@@ -15,92 +15,118 @@ def play_game():
 							(0, 4, 8),
 							(2, 4, 6)]
 
+	start = True
 	while True:
-		print('Your turn!')
-		move = input('Enter a position: ')
-		move = move.lower()
-		if move == 'exit':
-			print('Goodbye!')
-			return
-		# check for valid user input
-		if len(move) == 2:
-			int_row = int(move[0]) - 1
-			# check for valid row
-			if int_row != 0 and int_row != 1 and int_row != 2:
-				print('Invalid row!')
-				continue
-			# check for column
-			col = move[1]
-			if col != 'a' and col != 'b' and col != 'c':
-				print('Invalid column!')
-				continue
-			int_col = ord(col) - 97
-			# check that position is available
-			if taken[(3 * int_row) + int_col] != '-':
-				print('Position unavailable!')
-				continue
-			else:
-				# valid input
+		if not start:
+			move = input('Your turn!\n'\
+				         'Enter a position: ')
+			move = move.lower()
+			if move == 'exit':
+				print('Goodbye!')
+				return
+			if check_input(move, board):
 				num_moves += 1
-				taken[(3 * int_row) + int_col] = 'X'
-				print_board(taken)
+				int_row = int(move[0]) - 1
+				int_col = ord(move[1]) - 97
+				board[(3 * int_row) + int_col] = 'X'
+				print_board(board)
 
 				# check for winner
 				winner = check_for_winner(num_moves, int_col, int_row, 'X',
-					                      taken, winning_combinations)
+					                      board, winning_combinations)
 				if winner != '-':
 					# a user can only win after they take a turn
 					if winner == 'X':
 						print('Winner: X (you)\n'\
-							  'Congratulations, you are the winner!')
+							  'Congratulations, you won!')
 					# since they always make the first move, a tie game can
 					# only happen after the user takes their turn
 					if winner == 'C':
 						print("Winner: It's a tie!\n"\
 							  "Better luck next time!")
-					play_again = input('Play again? (Y/N): ')
-					if play_again == 'Y' or play_again == 'y':
-						# reset taken positions
-						taken = ['-', '-', '-', '-', '-', '-', '-', '-', '-']
-						# reset the move count and restart game
+
+					if play_again(board):
 						num_moves = 0
-						print_board(taken)
+						start = True
 						continue
 					else:
 						print('Goodbye!')
 						return
+
 				# make computer move if user's move does not produce win
-				print("Computer's turn!")
 				num_moves += 1
-				make_computer_move(taken)
+				print("Computer's turn!")
+				computer_move(board)
 				winner = check_for_winner(num_moves, int_col, int_row, 'O',
-					          taken, winning_combinations)
+					          board, winning_combinations)
 				# the computer can only win if it is its own turn
 				if winner == 'O':
 					print('Winner: O (computer)\n'\
 						  'Sorry, you lost. Better luck next time!')
+					if play_again(board):
+						num_moves = 0
+						start = True
+						continue
+			else:
+				continue
 		else:
-			print('Invalid input!')
-			continue
+			starter = determine_start()
+			start = False
+			if starter == 0:
+				num_moves += 1
+				print("Computer starts!")
+				computer_move(board)
+			else:
+				continue
 
 
-def make_computer_move(taken):
+def determine_start():
+	return random.randint(0, 1)
+
+
+def check_input(move, board):
+	if len(move) == 2:
+		if not move[0].isdigit():
+			print('Invalid row!')
+			return False
+		int_row = int(move[0]) - 1
+		# check for valid row
+		if int_row != 0 and int_row != 1 and int_row != 2:
+			print('Invalid row!')
+			return False
+		# check for valid column
+		col = move[1]
+		if col != 'a' and col != 'b' and col != 'c':
+			print('Invalid column!')
+			return False
+		int_col = ord(col) - 97
+		# check that position is available
+		if board[(3 * int_row) + int_col] != '-':
+			print('Position unavailable!')
+			return False
+		return True
+	print('Invalid input!')
+	return False
+
+
+def computer_move(board):
 	# if random index is taken, search iteratively for open spot
 	random_idx = random.randint(0, 8)
-	if taken[random_idx] != '-':
+	if board[random_idx] != '-':
 		for adder in range(1, 9):
 			new_idx = (random_idx + adder) % 9
-			if taken[new_idx] == '-':
+			if board[new_idx] == '-':
 				random_idx = new_idx
 				break
-	taken[random_idx] = 'O'
-	print_board(taken)
+	board[random_idx] = 'O'
+	print_board(board)
 
-def check_for_winner(moves, col, row, player, taken, winning_combos):
+
+def check_for_winner(moves, col, row, player, board, winning_combos):
 	# check for vertical win
 	for combo in winning_combos:
-		if (taken[combo[0]] == player and taken[combo[1]] == player and
-			taken[combo[2]] == player):
+		if (board[combo[0]] == player and board[combo[1]] == player and
+			board[combo[2]] == player):
 			return player
 
 	# if no winner is found and board is full, it is a Cat's game (tie)
@@ -110,7 +136,17 @@ def check_for_winner(moves, col, row, player, taken, winning_combos):
 	return '-'
 
 
-def print_board(taken):
+def play_again(board):
+	play_again = input('Play again? (Y/N): ')
+	if play_again == 'Y' or play_again == 'y':
+		# reset board positions
+		board[:] = ['-', '-', '-', '-', '-', '-', '-', '-', '-']
+		print_board(board)
+		return True
+	return False
+
+
+def print_board(board):
 	print('      A         B         C')
 	for row in range(0, 3):
 		print('           |         |         ')
@@ -118,31 +154,35 @@ def print_board(taken):
 		col_a = ' '
 		col_b = ' '
 		col_c = ' '
-		if taken[(3 * row)] != '-':
-			col_a = taken[(3 * row)]
-		if taken[(3 * row) + 1] != '-':
-			col_b = taken[(3 * row) + 1]
-		if taken[(3 * row) + 2] != '-':
-			col_c = taken[(3 * row) + 2]
+		if board[(3 * row)] != '-':
+			col_a = board[(3 * row)]
+		if board[(3 * row) + 1] != '-':
+			col_b = board[(3 * row) + 1]
+		if board[(3 * row) + 2] != '-':
+			col_c = board[(3 * row) + 2]
 
 		print('%c     %s    |    %s    |    %s    ' % (str(row + 1), col_a,
 			                                           col_b, col_c))
 		print('           |         |         ')
 		if row < 2:
 			print('  -------------------------------')
+	print('\n')
 
 
 def print_welcome():
 	print('\nWelcome to Tic Tac Toe.\n'\
-		  'Make a move by typing in the Row (1-3) and Column (A-C) of the\n'\
-		  'desired position in the terminal in the format RC.\n'\
+		  'When prompted, make a move by typing in the Row (1-3) and \n'\
+		  'Column (A-C) of the desired position in the format RC.\n'\
 		  'Here are some possible moves:\n'\
 		  '    > 3A\n'\
 		  '    > 2C\n'\
-		  '    > 1B\n'\
-		  'The game can be exited any time by holding Control+C\n'\
-		  'or typing "exit"\n\n'\
-		  'Below is the empty board. Have fun! \n\n')
+		  '    > 1B\n\n'\
+		  "*  Your moves are represented by 'X'\n"\
+		  "*  The computer's moves are represented by 'O'\n"\
+		  '*  The starting player is randomly chosen.\n'\
+		  '*  You may exit the game any time by holding Control+C\n'\
+		  '   or typing "exit"\n\n'\
+		  'Below is the empty board. Have fun!\n')
 
 
 if __name__ == '__main__':
