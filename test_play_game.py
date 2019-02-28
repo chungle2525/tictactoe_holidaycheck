8dab2ffc03a813bae716a550cd452291db76dbb7
chungle <chungle@umich.edu>
2019-02-28 16:00:08 -0600

    fixed import errors

--- test_play_game.py
+++ test_play_game.py
@@ -1,12 +1,15 @@
 import sys
-import StringIO
+import io
 from play_tictactoe import TicTacToeBoard
+from play_tictactoe import play_game
 
 def test_play_game():
 	# tie games where user starts
-	sys.stdin = StringIO.StringIO("1a\n1c\n3b\n2a\n2c\n")
-	play_tictactoe.play_game(3)
-	
+	sys.stdin = io.StringIO("1a\n1c\n3b\n2a\n2c\n")
+	board = TicTacToeBoard(3)
+	play_game(board)
+	assert board.user_score == 0
+	assert board.comp_score == 0
 
 if __name__ == "__main__":
 	print("Testing tic tac to game...")
