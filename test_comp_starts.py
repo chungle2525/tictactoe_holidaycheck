0c4bbcd3edded6484932dba533816d3c1167d504
chungle <chungle@umich.edu>
2019-02-28 19:18:40 -0600

    added tie test cases

--- test_comp_starts.py
+++ test_comp_starts.py
@@ -4,7 +4,29 @@ from play_tictactoe import TicTacToeBoard
 from play_tictactoe import play_game
 
 def test_comp_starts_tie():
-	pass
+	trying_tie()
+
+def test_comp_starts_comp_wins():
+	# computer wins quickly
+	opposite_corners()
+	adjacent_corners()
+	opposite_middles()
+	trying_win()
+
+def trying_tie():
+	board = TicTacToeBoard(3)
+	board.player = 'O'
+	sys.stdin = io.StringIO("1a\n2c\n3a\n3b\nn\n")
+	play_game(board)
+	assert board.user_score == 0
+	assert board.comp_score == 0
+
+	board = TicTacToeBoard(3)
+	board.player = 'O'
+	sys.stdin = io.StringIO("3c\n2c\n3a\n1b\nn\n")
+	play_game(board)
+	assert board.user_score == 0
+	assert board.comp_score == 0
 
 def opposite_corners():
 	"""Test if user takes opposite corners"""
@@ -55,7 +77,7 @@ def adjacent_corners():
 	assert board.comp_score == 1
 
 def opposite_middles():
-	"""Test if user takes adjacent corners"""
+	"""Test if user takes opposite middle positions"""
 	# vertical two
 	board = TicTacToeBoard(3)
 	board.player = 'O'
@@ -71,16 +93,25 @@ def opposite_middles():
 	assert board.user_score == 0
 	assert board.comp_score == 1
 
-def test_comp_starts_comp_wins():
-	opposite_corners()
-	adjacent_corners()
-	opposite_middles()
+def trying_win():
+	"""Test if user tries to beat the computer"""
+	board = TicTacToeBoard(3)
+	board.player = 'O'
+	sys.stdin = io.StringIO("2a\n3b\n1c\nn\n")
+	play_game(board)
+	assert board.user_score == 0
+	assert board.comp_score == 1
 
+	board = TicTacToeBoard(3)
+	board.player = 'O'
+	sys.stdin = io.StringIO("2c\n3b\n3c\nn\n")
+	play_game(board)
+	assert board.user_score == 0
+	assert board.comp_score == 1
 
 def test_play_game():
 	test_comp_starts_tie()
 	test_comp_starts_comp_wins()
-
 
 if __name__ == "__main__":
 	print("Testing tic tac to game...")
